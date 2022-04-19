82b589fb26504c95818f1c2ecf7a390b01d462aa
Vladimir Mironiuk <vladimironiuk@gmail.com>
2022-04-19 21:29:55 +0300

    Add Handler and Server for initial server setup

--- cmd/main.go
+++ cmd/main.go
@@ -1,3 +1,17 @@
 package main
 
-func main() {}
+import (
+	"log"
+
+	"github.com/VMironiuk/offlinescore"
+	"github.com/VMironiuk/offlinescore/pkg/handler"
+)
+
+func main() {
+	handlers := handler.NewHandler()
+	srv := new(offlinescore.Server)
+
+	if err := srv.Run("8000", handlers.InitRoutes()); err != nil {
+		log.Fatalf("an error is occured when running HTTP server: %s\n", err.Error())
+	}
+}
